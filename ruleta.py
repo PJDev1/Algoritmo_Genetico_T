92c3a0a97c376472f927bbabdcb78b522d485fd3
PJ <bernabemayapedrodejesus@gmail.com>
2024-01-21 00:28:43 -0600

    Primera Carga

---
+++ ruleta.py
@@ -0,0 +1,30 @@
+import random
+
+#def fitness(poblacion): 
+
+#
+
+#Funcion para generar individuo
+def genera_individuo():
+    count = 0
+    individuo = ""
+    while(count != 32):
+        temp = str(random.randint(0,1)) 
+        individuo += temp
+        count += 1
+    
+    return individuo
+
+tam_poblacion = int(input("Tam poblacion: "))
+poblacion = []
+count = 0
+
+#Llenar poblacion 
+while(count < tam_poblacion):
+   poblacion.append(genera_individuo())
+   count += 1
+
+print(poblacion)
+
+
+
