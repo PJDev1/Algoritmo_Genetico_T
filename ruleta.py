758c317bb22201462ac9e984a094c08511e4241e
PJ <bernabemayapedrodejesus@gmail.com>
2024-01-23 00:11:33 -0600

    Segunda carga

--- ruleta.py
+++ ruleta.py
@@ -1,20 +1,5 @@
 import random
 
-#def fitness(poblacion): 
-
-#
-
-#Funcion para generar individuo
-def genera_individuo():
-    count = 0
-    individuo = ""
-    while(count != 32):
-        temp = str(random.randint(0,1)) 
-        individuo += temp
-        count += 1
-    
-    return individuo
-
 tam_poblacion = int(input("Tam poblacion: "))
 poblacion = []
 count = 0
@@ -26,5 +11,13 @@ while(count < tam_poblacion):
 
 print(poblacion)
 
-
-
+#Funcion para generar individuo
+def genera_individuo():
+    count = 0
+    individuo = ""
+    while(count != 32):
+        temp = str(random.randint(0,1)) 
+        individuo += temp
+        count += 1
+    
+    return individuo
