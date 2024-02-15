7bcf326cbec8d3ca0ca460c6e98c5c12fd7f8353
PJ <bernabemayapedrodejesus@gmail.com>
2024-02-15 11:15:13 -0600

    muta y solo falta iterar

--- ruleta.py
+++ ruleta.py
@@ -12,6 +12,7 @@ a = float(input('a='))
 b = float(input('b='))
 r = float(input('Porcentaje de cruza(decimal): ')) 
 m = float(input('Porcentaje de muta(decimal): '))
+threshold = int(input('Ingrese n iteraciones: ')) 
 
 #Definicion de tamaño de los individuos (n bits)
 def numeroBits(inf, sup, k):
@@ -40,16 +41,6 @@ def fitness():
 def nSeleccionD():
     return round((1-r)*tam_poblacion)
 
-def paseDirecto():
-    for x in range(seleccion):
-        for x in pctFitness:
-            if nr <= x:
-                indice = pctFitness.index(x) #Saca el indice de la posicion de la ruleta para seleccionar el individuo de la poblacion original
-                pobNueva.append(pob[indice]) #Individuo seleccionado pasa directamente a la nueva poblacion
-                break
-
-        print("Individuo seleccionado de la poblacion original por su indice ----------->", indice, "\n")
-
 Fitness = np.zeros(tam_poblacion)
 Fitness = fitness()
 print(Fitness)
@@ -65,6 +56,8 @@ def torneo_p(p):
 cross_r = round((r*tam_poblacion)/2) #Indica cuantos pares NECESITO
 print(f'Necesito: {cross_r} par') #2
 
+mut_i = round((m*tam_poblacion))
+print(f'NInd a mutar: {mut_i}')
 #Cruza
 def cruza():
     punto_cruza = rnd.randint(0, t_ind-1)
@@ -81,28 +74,35 @@ def cruza():
 
 #Sacamos los n hijos necesarios y los metemos a la nueva poblacion
 poblacionNueva = np.empty((0,t_ind), dtype=int)
+n_pase_directo = nSeleccionD()
 for i in range(cross_r):
     parHijos = cruza() #Arreglo con 2 arreglos dentro
     poblacionNueva = np.append(poblacionNueva, parHijos, axis=0)
     
 print(f'NewP_array: \n{poblacionNueva}')
 
-i = 0
-n_pase_directo = nSeleccionD()
-print(f"Mientras {i} != {n_pase_directo}")
+
+i = 1
 while( i != n_pase_directo):
-    print("i=",i)
     rand_ind = rnd.choice(P)
-    print(f'rand_ind_type = {type(rand_ind)}')
     print(f'Nuevo individuo = ',rand_ind)
-    poblacionNueva = np.append(poblacionNueva, rand_ind)
+    poblacionNueva = np.append(poblacionNueva, rand_ind.reshape(1, -1), axis=0)
     i+=1
-print(f'Entraron {i} individuos nuevos a la población.')
-print(f'{i} de P. original + {r*10} por cruza')
+
+print(f'len({len(poblacionNueva)})')
 
 #Mutacion
 def mutacion():
     randInd = rnd.choice(poblacionNueva) #Seleccion de individuo de forma aleatoria de la poblacion
-    
+    print(f'Individuo a mutar = {randInd}')
+    bitMuta = rnd.randint(0, t_ind - 1)
+    if randInd[bitMuta] == 0:
+        randInd[bitMuta] = 1
+    else:
+        randInd[bitMuta] = 0
+    print("Individuo mutado: ", randInd)
+       
+for _ in range(mut_i):
+    mutacion()
 
-    
+#while (max(Fitness) < threshold):
